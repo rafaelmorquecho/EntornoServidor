1d27726cbf598e7a77bbfc8a468c0c561a7049a0
rafa <rlmorquecho@ono.com>
2017-09-18 10:41:18 +0200

    polimorfismo

--- Ejercicio01/src/ejercicio01/Principal.java
+++ Ejercicio01/src/ejercicio01/Principal.java
@@ -30,7 +30,6 @@ public class Principal {
         System.out.println("volumen " + cilindro2.getArea());
 
         //polimorfismo
-         //polimorfismo
         Circulo cilindro3 = new Cilindro(4, 3, "verde");
         System.out.println(cilindro3);
 
